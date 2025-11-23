2d20f9b55a859590eaecbc66d9fa4a91b3e1c58c
HANCORE <m.fattah-ahmad@gmx.de>
2025-11-23 10:42:45 +0100

    Add files via upload

---
+++ config/V3-min2/cava.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g;"
+
+# creating "dictionary" to replace char with bar
+i=0
+while [ $i -lt ${#bar} ]
+do
+    dict="${dict}s/$i/${bar:$i:1}/g;"
+    i=$((i=i+1))
+done
+
+
+# write cava config
+config_file="/tmp/polybar_cava_config"
+echo "
+[general]
+bars = 18
+sleep_timer = 0
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+" > $config_file
+
+# read stdiout from cava
+
+# read stdout from cava
+cava -p $config_file | while read -r line; do
+    # Detect "paused" output (all zeros, semicolon separated)
+    if [[ "$line" =~ ^(0;?)+$ ]]; then
+        echo ""     # Output empty line → Polybar hides module
+        continue
+    fi
+
+    echo "$line" | sed "$dict"
+done
+
